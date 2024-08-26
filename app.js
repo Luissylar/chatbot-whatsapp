2af9f743d86bc30c89d19aa562217713108f3faf
Luis Saldivar <56895393+Luissylar@users.noreply.github.com>
2024-08-25 23:09:02 -0500

    feat: Add new chatbot features and responses

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['Tu Orden a sido tomada con exito!, estaremos contactando contigo para la entrega de este.'])
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+const flowBienvenida = addKeyword([EVENTS.WELCOME]).addAnswer('🙌 Bienvenido a este chatbot De restaurante')
+const flowNotaVoz = addKeyword([EVENTS.VOICE_NOTE]).addAnswer('🎤 Recibí una nota de voz, Me alegra escucharte en unos momentos de dare una respuesta.')
+const flowUbicacion = addKeyword([EVENTS.LOCATION]).addAnswer('📍 Recibí una ubicación, Veo que estas cerca de unos ed nuestros locales, te puedo ayudar a llegar.')
+const flowMedia = addKeyword([EVENTS.MEDIA]).addAnswer('🖼️ Recibí una imagen, perfecto le avisare a unos de  los administradores para que te ayuden a resolverlo.')
+const flowDocumento = addKeyword([EVENTS.DOCUMENT]).addAnswer('📄 Recibí un documento, Te contactare con un administrador para que vea tu caso.')
+
+const flowMenu = addKeyword(['menu', 'cafe', 'comida']).addAnswer('🍔 Aquí tienes el menu', null, null, [flowSecundario])
+    .addAnswer(
     [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
+        '📄 Sopa: De fideos',
+        '📄 Plato fuerte: Pollo a la plancha',
+        '📄 Postre: Helado de chocolate',
+        '📄 Bebida: Jugo de naranja',
         '\n*2* Para siguiente paso.',
     ],
     null,
@@ -17,54 +26,36 @@ const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswe
     [flowSecundario]
 )
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+
+const flowGracias = addKeyword(['gracias', 'grac', 'hasta luego']).addAnswer(
     [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
+        '🙏 Gracias a ti por visitarnos, esperamos verte pronto',
     ],
     null,
     null,
-    [flowSecundario]
+    []
 )
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+const flowPrincipal = addKeyword(['hola', 'buen dia', 'buenas tardes', 'buenas noches', 'ola'])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot*, Soy el asistente virtual del restaurante Luigis')
     .addAnswer(
         [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
+            'te comparto los lo que puedo hacer por ti',
+            '👉 *menu* para ver el menu',
+            '👉 *deudas*  para ver su vista de deudas en el restaurante',
+            '👉 *pedidos* para ver los pedidos pendientes a su cuenta ',
+            '👉 *ubicacion* para ver nuestra ubicacion!',
         ],
         null,
         null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+        [flowMenu, flowGracias]
     )
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowPrincipal, flowNotaVoz, flowUbicacion, flowMedia, flowDocumento])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
